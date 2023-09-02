cacb143ff27df4e8b051bef4b4273b8a3e253f68
Vanila14 <139687493+Vanila14@users.noreply.github.com>
2023-09-03 00:13:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// №1
+for (let i = 2; i <= 12; i = i+2){
+    console.log(i)
+}
+    console.log('---------')
+// №2
+for (let a = 10; a >= 1; a = a-1){
+    console.log(a)
+}
+    console.log('---------')
+// №3
+    let ii = 2
+    while(ii < 12){
+        ii = ii+2
+        console.log(ii)
+    }
+    console.log('---------')
+    let aa = 11
+    while(aa > 1){
+        aa = aa-1
+        console.log(aa)
+    }
+
+
